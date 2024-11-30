b2ddbca9989ee6831580a14cd281d9bd0933eb1c
Christopher Crocker <chris.crocker7227@gmail.com>
2024-11-30 11:55:18 -0500

    add battery percentage

--- config.h
+++ config.h
@@ -65,5 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ datetime, "%s",           "%r " },
+	{ battery_perc,         " %s%%",       "BAT1" },
 };
